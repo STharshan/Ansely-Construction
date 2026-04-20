f5cd0e20b42dc0181a89836d0486369ffafb1c32
shandiru <msddiru@gmail.com>
2026-04-20 23:28:50 +0530

    Refine WhyChoose section layout and add to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Hero from '../components/Home/HeroSection'
 import CtaContactSection from '../components/Home/CtaContactSection'
 import CompletedProjects from '../components/Home/CompletedProjects'
+import WhyChoose from '../components/Home/WhyChoose'
 const Home = () => {
   return (
     <div>
       <Hero />
+      <WhyChoose />
       <CtaContactSection />
       <CompletedProjects />
     </div>
