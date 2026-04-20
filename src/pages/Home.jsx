63a08a5bd0fe0b90215c5c9bb67e3a5aa07ee03d
shandiru <msddiru@gmail.com>
2026-04-20 22:48:49 +0530

    add conatct

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/Home/HeroSection'
-
+import CtaContactSection from '../components/Home/CtaContactSection'
 const Home = () => {
   return (
     <div>
       <Hero />
+      <CtaContactSection />
     </div>
   )
 }
