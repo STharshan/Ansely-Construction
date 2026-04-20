e382af9af40d392896ba3d1417d2442d2319eaf1
shandiru <msddiru@gmail.com>
2026-04-20 23:06:56 +0530

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Home/HeroSection'
 import CtaContactSection from '../components/Home/CtaContactSection'
+import CompletedProjects from '../components/Home/CompletedProjects'
 const Home = () => {
   return (
     <div>
       <Hero />
       <CtaContactSection />
+      <CompletedProjects />
     </div>
   )
 }
